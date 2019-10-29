30f09900c17cddc42138945138ae5a22fc508465
Andrei Digori <andrei@digori.com>
2019-10-29 13:19:08 +0200

    add overloads for prompt

--- library/src/lib/services/prompt-popup.ts
+++ library/src/lib/services/prompt-popup.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { PromptComponent } from '../components/prompt/prompt.component';
 import { PromptPopupConfig } from '../configs/prompt-popup.config';
 
@@ -10,6 +11,8 @@ export class PromptPopup {
     private dialog: MatDialog
   ) { }
 
+  show(message?: string, initial?: string): Observable<string | string[]>;
+  show(config?: PromptPopupConfig): Observable<string | string[]>;
   show(message?: string | PromptPopupConfig, initial?: string) {
     if (typeof message === 'string') {
       message = {
